d50b3a8f276b0a6c2c883f595f9796f735c13bf3
not-aLi <animeblk2003@gmail.com>
2024-11-06 05:56:19 +0500

    Minor Fix

--- src/components/helpers/CapitilizeFirstLetter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CapitilizeFirstLetter = ( string ) => {
-  return string.charAt(0).toUpperCase() + string.substring(1);
-};
-
-export default CapitilizeFirstLetter;
